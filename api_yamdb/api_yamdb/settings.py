0c45861a52c6593026fc68a13409ca5ee71a94ed
ilia14042001@gmail.com <81635546+SobolyaRA@users.noreply.github.com>
2023-01-18 13:01:52 +0500

    v2

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = ['/app/static/']
+#STATICFILES_DIRS = ['/app/static/']
 
 
 AUTH_USER_MODEL = 'reviews.User'
